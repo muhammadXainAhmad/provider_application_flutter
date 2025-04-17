79d2fbf94bf8ef98a56377d20754f9181f3de664
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-04-17 15:40:12 +0500

    Now using provider in stateless widget to handle state

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_application/counter_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,36 +11,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomePage(),
+      home: ChangeNotifierProvider(
+        create: (context) => CounterProvider(),
+        child: HomePage(),
+      ),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home")),
-      body: Center(child: Text("$_count", style: TextStyle(fontSize: 25))),
+      body: Center(
+        child: Text(
+          "${Provider.of<CounterProvider>(context, listen: true).getCount()}",
+          style: TextStyle(fontSize: 25),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          setState(() {
-            _count++;
-          });
+          Provider.of<CounterProvider>(context, listen: false).incrementCount();
         },
       ),
     );
